a5b3dffeb671b1c9dff2b53119fe505423deb7e6
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-08-11 10:07:03 +0530

    Thank You for Choosing to Learn from in28Minutes

---
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World"; 
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World"); 
+	}
+	
+}
