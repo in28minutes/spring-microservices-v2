cef57eab268a2ac7aa7bc4390c6b57b8f69e4639
Ranga Karanam <ranga@Rangas-MacBook-Pro-2.local>
2022-08-16 15:27:39 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -27,6 +37,21 @@ public class HelloWorldController {
 		return new HelloWorldBean(String.format("Hello World, %s", name)); 
 	}
 
-	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+		
+		//return "Hello World V2"; 
+		
+		//1:
+		//2:
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
+	}
+
 	
 }
